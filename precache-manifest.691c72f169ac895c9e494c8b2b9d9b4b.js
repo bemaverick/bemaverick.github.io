c582923e07af282fd376920661a3bb4395deb051
smorykon <smorykon@s-pro.io>
2019-11-03 03:35:25 +0200

    Updates

---
+++ precache-manifest.691c72f169ac895c9e494c8b2b9d9b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3362f5f70e8bd0e35e7a77118cde88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1aeb7e258053f52935",
+    "url": "/static/css/2.fb64e31e.chunk.css"
+  },
+  {
+    "revision": "a73f6da4716869b5bbd2",
+    "url": "/static/css/main.ba7f6d56.chunk.css"
+  },
+  {
+    "revision": "2d1aeb7e258053f52935",
+    "url": "/static/js/2.d8058fae.chunk.js"
+  },
+  {
+    "revision": "a73f6da4716869b5bbd2",
+    "url": "/static/js/main.ab5932fb.chunk.js"
+  },
+  {
+    "revision": "e1cf17f4ade785e9d5fb",
+    "url": "/static/js/runtime-main.af0c0a2b.js"
+  }
+]);
