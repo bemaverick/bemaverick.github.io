8777abe4f1b0dbe3a242f690d3eed353e522661e
smorykon <smorykon@s-pro.io>
2019-11-03 03:17:31 +0200

    Updates

---
+++ precache-manifest.b89dcb11dbd832230a4175d148f4bc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c57e9c75a67d97ab57cd51cd3bec555",
+    "url": "/bemaverick/index.html"
+  },
+  {
+    "revision": "f7ae8683382cadb88572",
+    "url": "/bemaverick/static/css/2.fb64e31e.chunk.css"
+  },
+  {
+    "revision": "995b3fe8d6d75b92bfef",
+    "url": "/bemaverick/static/css/main.ba7f6d56.chunk.css"
+  },
+  {
+    "revision": "f7ae8683382cadb88572",
+    "url": "/bemaverick/static/js/2.f750505c.chunk.js"
+  },
+  {
+    "revision": "995b3fe8d6d75b92bfef",
+    "url": "/bemaverick/static/js/main.51938ad6.chunk.js"
+  },
+  {
+    "revision": "7d22e99cd82f9551422f",
+    "url": "/bemaverick/static/js/runtime-main.3db67b04.js"
+  }
+]);
